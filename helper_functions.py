0e41cfe2d99387a735424e70ae5bc8f40b2fc25a
Fardin Syed <fardin.syed99@gmail.com>
2019-05-14 16:00:20 -0400

    Fixed typo

--- helper_functions.py
+++ helper_functions.py
@@ -248,7 +248,7 @@ def ClassifyTopQuark(tree, entry, t_indices):
         d_index1 = TraverseSelfDecay(tree, entry, int(d1.GetValue(index)))
         d_index2 = TraverseSelfDecay(tree, entry, int(d2.GetValue(index)))
         assert np.abs(pids.GetValue(d_index1)) == 24 \
-                        or np.abs(pids.GetValue(d_index2])) == 24,
+                        or np.abs(pids.GetValue(d_index2)) == 24,
                         "No W quark found in decay of top quark"
         # Find the children for the W quark
         if np.abs(pids[d_index1]) == 24:
