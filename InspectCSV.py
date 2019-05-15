e827080800e68a2017eecd2a3dd328690dc508f0
Fardin Syed <fardin.syed99@gmail.com>
2019-05-15 15:01:38 -0400

    Made plots for csv file

--- InspectCSV.py
+++ InspectCSV.py
@@ -1,14 +1,17 @@
 # Use this script to inspect the CSV file outputted from root2csv.py
 import pandas as pd
 import matplotlib.pyplot as plt
+from features import column_names
 
-df = pd.read_csv("csv/topreco.csv")
+df = pd.read_csv("csv/topreco.csv", names=column_names)
 columns = df.columns
 shape = df.shape
 print("Shape of Data: {}".format(shape))
 for i in range(len(columns)):
-    plt.hist(data[columns[i]], bins=100, label=columns[i])
+    plt.clf()
+    fig = plt.figure()
+    plt.hist(df[column_names[i]], bins=100, label=column_names[i])
     plt.legend()
     plt.title("Number of Events: {}".format(shape[0]))
     plt.ylabel("Counts")
-    plt.savefig("Plots/{}".format(columns[i]))
+    plt.savefig("Plots/{}.png".format(column_names[i]))
--- features.py
+++ features.py
@@ -1,5 +1,5 @@
 column_names = ["jets_n", "bjets_n",
-"lep.Px()", "lep.Py()", "lep.Pz()", "lep.E()", "met_met", "met_phi",
+"lep.Px", "lep.Py", "lep.Pz", "lep.E", "met_met", "met_phi",
 "jet0 P_x",  "jet0 P_y",  "jet0 P_z",  "jet0 E",  "jet0 M",  "jet0 BTag",
 "jet1 P_x",  "jet1 P_y",  "jet1 P_z",  "jet1 E",  "jet1 M",  "jet1 BTag",
 "jet2 P_x",  "jet2 P_y",  "jet2 P_z",  "jet2 E",  "jet2 M",  "jet2 BTag",
