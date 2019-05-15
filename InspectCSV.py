a063228448bacb47a8d4fd2b522e18382562faf1
Fardin Syed <fardin.syed99@gmail.com>
2019-05-15 14:45:10 -0400

    Removed column names

--- InspectCSV.py
+++ InspectCSV.py
@@ -2,5 +2,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("csv/topreco.csv")
-print(data.columns)
+df = pd.read_csv("csv/topreco.csv")
+columns = df.columns
+shape = df.shape
+print("Shape of Data: {}".format(shape))
+for i in range(len(columns)):
+    plt.hist(data[columns[i]], bins=100, label=columns[i])
+    plt.legend()
+    plt.title("Number of Events: {}".format(shape[0]))
+    plt.ylabel("Counts")
+    plt.savefig("Plots/{}".format(columns[i]))
