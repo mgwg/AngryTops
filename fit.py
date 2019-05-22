1b2d5ef0afe75b707c4ba767179493a4ab92fc66
Fardin Syed <fardin.syed99@gmail.com>
2019-05-22 16:06:07 -0400

    prepped fit for debugging

--- fit.py
+++ fit.py
@@ -21,7 +21,7 @@ m_W = 80.4
 m_b = 4.95
 max_momentum = 1000.
 np.set_printoptions( precision=3, suppress=True, linewidth=250 )
-infilename = "csv/topreco_augmented1.csv"
+infilename = "csv/topreco.csv"
 model_filename  = "{}/simple_model.h5".format(training_dir)
 if len(sys.argv) > 1: infilename = sys.argv[1]
 
