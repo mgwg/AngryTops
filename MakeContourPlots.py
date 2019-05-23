92dbe857c16d40eb17ff88446a9514c298593a85
Fardin Syed <fardin.syed99@gmail.com>
2019-05-23 14:52:32 -0400

    typo

--- MakeContourPlots.py
+++ MakeContourPlots.py
@@ -1,5 +1,6 @@
 from ROOT import TCanvas, TFile, TProfile, TNtuple, TH1F, TH2F
 from ROOT import gROOT, gBenchmark, gRandom, gSystem, Double
+from features import *
 
 def draw_contour(attribute_name, treename='fitted.root'):
     # Create a new canvas, and customize it.
