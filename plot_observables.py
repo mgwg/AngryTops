ac3242bc7d17ef2275ebfdf6d78198e9f11abaa7
Fardin Syed <fardin.syed99@gmail.com>
2019-06-04 15:28:23 -0400

    Changed label location

--- plot_observables.py
+++ plot_observables.py
@@ -400,7 +400,7 @@ def plot_correlations(hist_name):
     l.SetNDC()
     l.SetTextFont(42)
     l.SetTextColor(kBlack)
-    l.DrawLatex( 0.7, 0.8, "Corr Coeff: %.2f" % corr )
+    l.DrawLatex( 0.7, -0.8, "Corr Coeff: %.2f" % corr )
 
     gPad.RedrawAxis()
 
