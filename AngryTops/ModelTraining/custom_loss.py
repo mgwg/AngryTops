0030a37a7f3a009ba910755b67a01c9b883f94e4
Fardin Syed <fardin.syed99@gmail.com>
2019-06-12 14:22:54 -0400

    Tried (and failed) to use a KL Divergence Loss Function

--- AngryTops/ModelTraining/custom_loss.py
+++ AngryTops/ModelTraining/custom_loss.py
@@ -6,8 +6,17 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import *
 from tensorflow.keras.regularizers import *
+import tensorflow.keras.backend as K
 
 kl_divergence = keras.losses.kullback_leibler_divergence
+
+def kl(true, false):
+    arr = np.log(true / false)
+    arr[arr != arr] = 0
+    arr[np.isinf(arr)] = 0
+    print(true)
+    return np.sum(true * arr)
+
 
 def kl_loss(y_true, y_pred):
     """
@@ -26,21 +35,21 @@ def kl_loss(y_true, y_pred):
     Step 3: Calculate the kl loss for each of these and take the (possibly
     weighted) sum for get the loss.
     """
-    y_true = tf.constant(y_true, dtype=tf.dtypes.float64)
-    y_pred = tf.constant(y_pred, dtype=tf.dtypes.float64)
-    y_true = tf.reshape(y_true, shape=[y_true.shape[0], y_true.shape[1] * y_true.shape[2]])
-    y_pred = tf.reshape(y_pred, shape=[y_pred.shape[0], y_pred.shape[1] * y_pred.shape[2]])
+    #y_true = tf.constant(y_true, dtype=tf.dtypes.float64)
+    #y_pred = tf.constant(y_pred, dtype=tf.dtypes.float64)
+    #y_true = tf.reshape(y_true, shape=[y_true.shape[0], y_true.shape[1] * y_true.shape[2]])
+    #y_pred = tf.reshape(y_pred, shape=[y_pred.shape[0], y_pred.shape[1] * y_pred.shape[2]])
     loss = 0
+    if y_true.shape[-1] is None:
+        return 0.0
     for i in range(y_true.shape[-1]):
-        hist_true = tf.histogram_fixed_width(y_true[:,i],
-        [tf.math.reduce_min(y_true).numpy(), tf.math.reduce_max(y_true).numpy()], nbins=10)
-        hist_pred = tf.histogram_fixed_width(y_pred[:,i],
-        [tf.math.reduce_min(y_pred).numpy(), tf.math.reduce_max(y_pred).numpy()], nbins=10)
-        loss += kl_divergence(hist_true, hist_pred)
+        hist_true = np.histogram(y_true[:,i], bins=10, density=True)
+        hist_pred = np.histogram(y_pred[:,i], bins=10, density=True)
+        loss += kl(hist_true[0], hist_pred[0])
     return loss
 
 if __name__ == "__main__":
     fh = np.load('/Users/fardinsyed/Desktop/Top_Quark_Project/AngryTops/CheckPoints/archived/keep/training_5_minmax/predictions.npz')
     pred = fh['pred']
     true = fh['true']
-    pred(kl_loss(true[:32], pred[:32]))
+    print(kl_loss(true[:32], pred[:32]))
