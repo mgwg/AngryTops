36b1336496548b4cac43d0ad4abf94dc5e32f510
Fardin Syed <fardin.syed99@gmail.com>
2019-07-08 12:26:12 -0400

    Prepped two new searches

--- AngryTops/HyperParameterSearch/param_spaces.py
+++ AngryTops/HyperParameterSearch/param_spaces.py
@@ -33,4 +33,48 @@ space1 = {
 'rec_weight': hp.uniform('rec_weight', 0, 1)
 }
 
-parameter_spaces = {'space0': space0, 'space1': space1}
+space2 = {
+'learn_rate': hp.uniform('learn_rate', 10e-6, 10e-4),
+'size1': hp.quniform('size1', 1, 200, 1),
+'size2': hp.quniform('size2', 1, 200, 1),
+'size3': hp.quniform('size3', 1, 200, 1),
+'size4': hp.quniform('size4', 1, 200, 1),
+'size5': hp.quniform('size5', 1, 200, 1),
+'size6': hp.quniform('size6', 1, 200, 1),
+'size7': hp.quniform('size7', 1, 200, 1),
+'size8': hp.quniform('size8', 1, 200, 1),
+'act1': hp.choice('act1', ['relu', 'elu', 'tanh']),
+'act2': hp.choice('act2', ['relu', 'elu', 'tanh']),
+'act3': hp.choice('act3', ['relu', 'elu', 'tanh']),
+'act4': hp.choice('act4', ['relu', 'elu', 'tanh']),
+'act5': hp.choice('act5', ['relu', 'elu', 'tanh']),
+'act6': hp.choice('act6', ['relu', 'elu', 'tanh']),
+'kernel_reg1': hp.uniform('kernel_reg1', 0, 1),
+'kernel_reg2': hp.uniform('kernel_reg2', 0, 1),
+'kernel_reg3': hp.uniform('kernel_reg3', 0, 1),
+'kernel_reg4': hp.uniform('kernel_reg4', 0, 1),
+'kernel_reg5': hp.uniform('kernel_reg5', 0, 1)
+}
+
+space3 = {
+'learn_rate': hp.uniform('learn_rate', 10e-6, 10e-4),
+'size1': hp.quniform('size1', 1, 200, 1),
+'size2': hp.quniform('size2', 1, 200, 1),
+'size3': hp.quniform('size3', 1, 200, 1),
+'size4': hp.quniform('size4', 1, 200, 1),
+'size5': hp.quniform('size5', 1, 200, 1),
+'size6': hp.quniform('size6', 1, 200, 1),
+'size7': hp.quniform('size7', 1, 200, 1),
+'act1': hp.choice('act1', ['relu', 'elu', 'tanh']),
+'act2': hp.choice('act2', ['relu', 'elu', 'tanh']),
+'act3': hp.choice('act3', ['relu', 'elu', 'tanh']),
+'act4': hp.choice('act4', ['relu', 'elu', 'tanh']),
+'act5': hp.choice('act5', ['relu', 'elu', 'tanh']),
+'kernel_reg1': hp.uniform('kernel_reg1', 0, 1),
+'kernel_reg2': hp.uniform('kernel_reg2', 0, 1),
+'kernel_reg3': hp.uniform('kernel_reg3', 0, 1),
+'kernel_reg4': hp.uniform('kernel_reg4', 0, 1),
+'kernel_reg5': hp.uniform('kernel_reg5', 0, 1)
+}
+
+parameter_spaces = {'space0': space0, 'space1': space1, 'space2': space2}
--- AngryTops/HyperParameterSearch/test_models.py
+++ AngryTops/HyperParameterSearch/test_models.py
@@ -82,4 +82,88 @@ def test_model1(config):
 
     return model
 
-test_models = {'test_model0': test_model0, 'test_model1': test_model1}
+def test_model2(config):
+    """A denser version of model_multi"""
+    kernel_reg1 = config['kernel_reg1']
+    kernel_reg2 = config['kernel_reg2']
+    kernel_reg3 = config['kernel_reg3']
+    kernel_reg4 = config['kernel_reg4']
+    kernel_reg5 = config['kernel_reg5']
+
+    input_jets = Input(shape = (20,), name="input_jets")
+    input_lep = Input(shape=(5,), name="input_lep")
+    # Jets
+    x_jets = Reshape(target_shape=(5,4))(input_jets)
+    x_jets = LSTM(int(config['size1']), return_sequences=True, kernel_regularizer=l2(kernel_reg1))(x_jets)
+    x_jets = LSTM(int(config['size2']), return_sequences=False, kernel_regularizer=l2(kernel_reg2))(x_jets)
+    x_jets = Dense(int(config['size3']), activation=config['act1'], kernel_regularizer=l2(kernel_reg3))(x_jets)
+    x_jets = Dense(int(config['size4']), activation=config['act2'])(x_jets)
+    x_jets = keras.Model(inputs=input_jets, outputs=x_jets)
+
+    # Lep
+    x_lep = Dense(int(config['size5']), activation=config['act3'])(input_lep)
+    x_lep = Dense(int(config['size6']), activation=config['act4'])(x_lep)
+    x_lep = keras.Model(inputs=input_lep, outputs=x_lep)
+
+    # Combine them
+    combined = concatenate([x_lep.output, x_jets.output], axis=1)
+
+    # Apply some more layers to combined data set
+    final = BatchNormalization()(combined)
+    final = Dense(int(config['size7']), activation=config['act5'], kernel_regularizer=l2(kernel_reg4))(final)
+    final = Dense(int(config['size8']), activation=config['act6'], kernel_regularizer=l2(kernel_reg5))(final)
+    final = Dense(18, activation='linear')(final)
+    final = Reshape(target_shape=(6,3))(final)
+
+    # Make final model
+    model = keras.Model(inputs=[x_lep.input, x_jets.input], outputs=final)
+
+    optimizer = tf.keras.optimizers.Adam(config['learn_rate'])
+    model.compile(optimizer=optimizer, loss='mse', metrics=['mae', 'mse'])
+
+    return model
+
+def test_model3(config):
+    """A denser version of model_multi"""
+    kernel_reg1 = config['kernel_reg1']
+    kernel_reg2 = config['kernel_reg2']
+    kernel_reg3 = config['kernel_reg3']
+    kernel_reg4 = config['kernel_reg4']
+    kernel_reg5 = config['kernel_reg5']
+
+    input_jets = Input(shape = (20,), name="input_jets")
+    input_lep = Input(shape=(5,), name="input_lep")
+
+    # Jets
+    x_jets = Reshape(target_shape=(5,4))(x_jets)
+    x_jets = Dense(int(config['size1']), activation=config['act1'], kernel_regularizer=l2(kernel_reg1))(input_jets)
+    x_jets = LSTM(int(config['size2']), return_sequences=True, kernel_regularizer=l2(kernel_reg2))(x_jets)
+    x_jets = LSTM(int(config['size3']), return_sequences=False, kernel_regularizer=l2(kernel_reg3))(x_jets)
+    x_jets = BatchNormalization()(x_jets)
+    x_jets = Dense(int(config['size4']), activation=config['act2'])(x_jets)
+    x_jets = keras.Model(inputs=input_jets, outputs=x_jets)
+
+    # Lep
+    x_lep = Dense(int(config['size5']), activation=config['act3'], kernel_regularizer=l2(kernel_reg4))(input_lep)
+    x_lep = BatchNormalization()(x_lep)
+    x_lep = Dense(int(config['size6']), activation=config['act4'])(x_lep)
+    x_lep = keras.Model(inputs=input_lep, outputs=x_lep)
+
+    # Combine them
+    combined = concatenate([x_lep.output, x_jets.output], axis=1)
+
+    # Apply some more layers to combined data set
+    final = Dense(int(config['size7']), activation=config['act5'], kernel_regularizer=l2(kernel_reg5))(combined)
+    final = Dense(18)(final)
+    final = Reshape(target_shape=(6,3))(final)
+
+    # Make final model
+    model = keras.Model(inputs=[x_lep.input, x_jets.input], outputs=final)
+
+    optimizer = tf.keras.optimizers.Adam(config['learn_rate'])
+    model.compile(optimizer=optimizer, loss='mse', metrics=['mae', 'mse'])
+
+    return model
+
+test_models = {'test_model0': test_model0, 'test_model1': test_model1,
+               'test_model2': test_model2, 'test_model3': test_model3}
