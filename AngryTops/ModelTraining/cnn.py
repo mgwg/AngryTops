7861288497852780e14a36d434ebe59e18b22cb1
Fardin Syed <fardin.syed99@gmail.com>
2019-07-10 11:18:11 -0400

    Added optimized cnn model

--- AngryTops/ModelTraining/cnn.py
+++ AngryTops/ModelTraining/cnn.py
@@ -77,7 +77,32 @@ def cnn3(**kwargs):
     model.compile(optimizer=optimizer, loss='mse', metrics=['mae', 'mse'])
     return model
 
-cnn_models = {'cnn1': cnn1, 'cnn2': cnn2, 'cnn3': cnn3}
+def cnn2_hypopt(**kwargs):
+    """A simple convolutional network model"""
+    config = {'act1': 'tanh', 'act2': 'tanh', 'act3': 'tanh',
+    'learn_rate': 0.0007631163113143247, 'size1': 127.0, 'size2': 158.0,
+    'size3': 181.0}
+    model = keras.models.Sequential()
+    model.add(Dense(256, input_shape=(36,), activation='relu'))
+    model.add(Reshape(target_shape=(8,8,4)))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D((2, 2), strides=2))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D((2, 2), strides=2))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D((2, 2), strides=2))
+    model.add(Flatten())
+    model.add(Dense(int(config['size1']), activation=config['act1']))
+    model.add(Dense(int(config['size2']), activation=config['act2']))
+    model.add(Dense(int(config['size3']), activation=config['act3']))
+    model.add(Dense(18))
+    model.add(Reshape(target_shape=(6,3)))
+
+    optimizer = tf.keras.optimizers.Adam(config['learn_rate'])
+    model.compile(optimizer=optimizer, loss='mse', metrics=['mae', 'mse'])
+    return model
+
+cnn_models = {'cnn1': cnn1, 'cnn2': cnn2, 'cnn3': cnn3, 'cnn2_hypopt':cnn2_hypopt}
 
 if __name__ == "__main__":
     model = cnn3()
