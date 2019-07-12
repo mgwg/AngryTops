877f17d10fa9fc2e0f78f133cb52bde9a81836b0
Fardin Syed <fsyed@huron.physics.utoronto.ca>
2019-07-12 14:57:53 -0400

    updated space and test_model dictionaries

--- AngryTops/HyperParameterSearch/param_spaces.py
+++ AngryTops/HyperParameterSearch/param_spaces.py
@@ -78,7 +78,7 @@ space3 = {
 'kernel_reg5': hp.uniform('kernel_reg5', 0, 1)
 }
 
-space3 = {
+space4 = {
 'learn_rate': hp.uniform('learn_rate', 10e-6, 10e-4),
 'size1': hp.quniform('size1', 1, 200, 1),
 'size2': hp.quniform('size2', 1, 200, 1),
@@ -112,5 +112,5 @@ cnn_space2 = {
 
 parameter_spaces = {
 'space0': space0, 'space1': space1, 'space2': space2, 'space3': space3,
-'cnn_space1': cnn_space1, 'cnn_space2': cnn_space2
+    'cnn_space1': cnn_space1, 'cnn_space2': cnn_space2, 'space4': space4,
 }
