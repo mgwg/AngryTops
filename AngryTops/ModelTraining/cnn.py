761ec67e20d69050f63b456ff46ccb4008b17fd6
Fardin Syed <fardin.syed99@gmail.com>
2019-07-16 10:47:49 -0400

    Created CNN model w/ dropout

--- AngryTops/ModelTraining/cnn.py
+++ AngryTops/ModelTraining/cnn.py
@@ -131,8 +131,39 @@ def cnn4(**kwargs):
     model.compile(optimizer=optimizer, loss='mse', metrics=['mae', 'mse'])
     return model
 
+def cnn5(**kwargs):
+    """A simple convolutional network model w/ dropout"""
+    model = keras.models.Sequential()
+    model.add(Dense(1024, input_shape=(36,), activation='relu'))
+    model.add(Reshape(target_shape=(16,16,4)))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D((2, 2), strides=2))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D((2, 2), strides=2))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D((2, 2), strides=2))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(Conv2D(256, (3, 3), activation='relu', padding="same"))
+    model.add(MaxPooling2D((2, 2), strides=2))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dense(127, activation='tanh'))
+    model.add(Dense(158, activation='tanh'))
+    model.add(Dense(181, activation='tanh'))
+    model.add(Dense(18))
+    model.add(Reshape(target_shape=(6,3)))
+
+    optimizer = tf.keras.optimizers.Adam(10e-5)
+    model.compile(optimizer=optimizer, loss='mse', metrics=['mae', 'mse'])
+    return model
+
 cnn_models = {'cnn1': cnn1, 'cnn2': cnn2, 'cnn3': cnn3, 'cnn2_hypopt':cnn2_hypopt,
-              'cnn4': cnn4}
+              'cnn4': cnn4, 'cnn5': cnn5}
 
 if __name__ == "__main__":
     model = cnn4()
