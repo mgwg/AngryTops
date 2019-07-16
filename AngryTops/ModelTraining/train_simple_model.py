89aeea4849b4301f87e63007db3f5f987c8e8bb0
Fardin Syed <fardin.syed99@gmail.com>
2019-07-16 15:04:02 -0400

    Fixed syntax error

--- AngryTops/ModelTraining/train_simple_model.py
+++ AngryTops/ModelTraining/train_simple_model.py
@@ -54,8 +54,7 @@ def train_model(model_name, train_dir, csv_file, log_training=True, load_model=F
     # LOADING / PRE-PROCESSING DATA
     (training_input, training_output), (testing_input, testing_output), \
     (jets_scalar, lep_scalar, output_scalar), (event_training, event_testing) \
-                           = get_input_output(input_filename=csv_file, **kwargs), \
-                                        (training_event_info, testing_event_info)
+    (training_event_info, testing_event_info) = get_input_output(input_filename=csv_file, **kwargs)
 
     ###########################################################################
     # BUILDING / TRAINING MODEL
