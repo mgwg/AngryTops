b4ff02109b3ff4923718b752000a808630a9801e
Fardin Syed <fardin.syed99@gmail.com>
2019-07-23 12:27:20 -0400

    Fixed bug in retraining pipeline

--- AngryTops/PostTraining_Analysis/retrain.py
+++ AngryTops/PostTraining_Analysis/retrain.py
@@ -20,7 +20,7 @@ print("Isolating for events that failed the pT cutoff")
 select_pT_events()
 
 # Retrain Model on the selected events
-train_model("bidirectional_LSTM1", "Retrained_LSTM2", "b_had_pT.csv",
-log_training=False, load_model=False, EPOCHES=100, BATCH_SIZE=32,
+train_model("bidirectional_LSTM1", "Retrained_LSTM", "b_had_pT.csv",
+log_training=False, load_model=True, EPOCHES=100, BATCH_SIZE=32,
 scaling='minmax', rep='pxpypzEM', multi_input=False, sort_jets=False,
 shuffle=False, training_split=0.5)
