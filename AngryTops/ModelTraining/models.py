84aeb5d6d8c3dc81dd6a596db6848a69fe790365
Fardin Syed <fsyed@huron.physics.utoronto.ca>
2019-08-02 14:27:06 -0400

    Fixed typo

--- AngryTops/ModelTraining/models.py
+++ AngryTops/ModelTraining/models.py
@@ -15,7 +15,7 @@ custom_metrics={"weighted_MSE1": weighted_MSE1, "weighted_MSE2": weighted_MSE2,
 "w_HAD":w_HAD, "w_LEP":w_LEP, "b_HAD":b_HAD, "b_LEP":b_LEP, "t_HAD":t_HAD,
 "t_LEP":t_LEP, "pT_loss":pT_loss, 'pTetaphi_Loss':pTetaphi_Loss, 'mmd_loss':mmd_loss}
 metrics = ['mae', 'mse', weighted_MSE1, weighted_MSE2, w_HAD, w_LEP, b_HAD,
-           b_LEP, t_HAD, t_LEP, pT_loss, pTetaphi_Loss, 'mmd_loss':mmd_loss]
+           b_LEP, t_HAD, t_LEP, pT_loss, pTetaphi_Loss, mmd_loss]
 losses = {"mse":"mse", "weighted_MSE1": weighted_MSE1,
           "weighted_MSE2": weighted_MSE2, "pT_loss":pT_loss,
           "pTetaphi_Loss":pTetaphi_Loss, 'mmd_loss':mmd_loss}
