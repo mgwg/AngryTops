32ff56d3bf140b66b6fe943e0a117f8981be645f
Fardin Syed <fardin.syed99@gmail.com>
2019-08-19 16:05:56 -0400

    Updated axes limits on the plots

--- AngryTops/Experimental/Retrain.py
+++ AngryTops/Experimental/Retrain.py
@@ -1,33 +1,51 @@
 import tensorflow as tf
 from tensorflow.keras.layers import *
 from tensorflow.keras.regularizers import *
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from AngryTops.features import *
 from AngryTops.ModelTraining.single_output_models import *
+from AngryTops.ModelTraining.FormatInputOutput import get_input_output
+from AngryTops.ModelTraining.models import *
+from AngryTops.ModelTraining.custom_loss import *
 
 EPOCHES = 100
 BATCH_SIZE=32
 (training_input, training_output), (testing_input, testing_output), \
 (jets_scalar, lep_scalar, output_scalar), (event_training, event_testing) = \
-get_input_output(input_filename="topreco_5dec3.csv", scaling='minmax',
+get_input_output(input_filename="topreco_5dec2.csv", scaling='minmax',
 rep="experimental", multi_input=False, sort_jets=False, particle="W_lep_cart")
 
 # Make + Train Models
-WLEP_model = BDLSTM_model()
+WLEP_model = BDLSTM_model(metrics, losses)
 cp_callback = ModelCheckpoint(".", monitor='val_loss', save_weights_only=True, verbose=1)
 early_stopping = EarlyStopping(monitor='val_loss', patience=0)
-history = model.fit(training_input, training_output,  epochs=EPOCHES,
-                    batch_size=BATCH_SIZE, validation_split=0.1,
-                    callbacks = [early_stopping, cp_callback]
-                    )
-model.save('WLep_Model.h5')
+try:
+    history = model.fit(training_input, training_output,  epochs=EPOCHES,
+                        batch_size=BATCH_SIZE, validation_split=0.1,
+                        callbacks = [early_stopping, cp_callback]
+                        )
+except Exception as e:
+    print("Training Interrupted")
+
+WLEP_model.save('WLep_Model.h5')
 
 # Use the WLEP model to update the training/testing data
 (training_input, training_output), (testing_input, testing_output), \
 (jets_scalar, lep_scalar, output_scalar), (event_training, event_testing) = \
-get_input_output(input_filename="topreco_5dec3.csv", scaling='minmax',
+get_input_output(input_filename="topreco_5dec2.csv", scaling='minmax',
 rep="experimental", multi_input=False, sort_jets=False, particle="b_lep_cart")
 
-# 
+# Add predictions from previous model to the input
+pred_train = model.predict(training_input)
+pred_test = model.predict(testing_input)
+filler_train = np.zeros(shape=(pred_train.shape[0]))
+filler_test = np.zeros(shape=(pred_test.shape[0]))
+pred_train = np.c_[pred_train, filler_train]
+pred_test = np.c_[pred_test, filler_test]
+training_input = np.c_[training_input, pred_train]
+testing_input = np.c_[testing_input, pred_test]
+print(training_input.shape)
+print(testing_input.shape)
 
 
 def WLEP_model():
@@ -87,13 +105,3 @@ def bHAD_model():
     model.compile(optimizer=optimizer, loss='mse', metrics=['mse', 'mae'])
 
     return model
-
-
-
-
-if __name__ == "__main__":
-    # Make first model
-    model = BDLSTM_model()
-    print(model.summary())
-
-    # Load data for first model
