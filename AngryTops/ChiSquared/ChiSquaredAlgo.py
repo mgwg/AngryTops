465f97ff550153593931e7ff172640a4804f4621
Fardin Syed <fsyed@huron.physics.utoronto.ca>
2019-08-23 16:09:53 -0400

    Debugged Chi2 script

--- AngryTops/ChiSquared/ChiSquaredAlgo.py
+++ AngryTops/ChiSquared/ChiSquaredAlgo.py
@@ -2,6 +2,7 @@ import numpy as np
 from ROOT import *
 import itertools
 from AngryTops.features import *
+import pickle
 
 # Mass and mass uncertainties for W and t in GeV
 mW = 80.4
@@ -15,11 +16,13 @@ def MakeNeutrino(lep_Px, lep_Py, lep_Pz, met, met_phi):
     transverse energy and missing azimuthal energy
     """
     # v refers to the the neutrino
+    v_pT = met
     v_px = met * np.cos(met_phi)
     v_py = met * np.sin(met_phi)
 
     # lepton already made by reco or particle levels:
-    lep = TLorentzVector().SetPxPyPzE(lep_Px, lep_Py, lep_Pz, 0)
+    lep = TLorentzVector()
+    lep.SetPxPyPzE(lep_Px, lep_Py, lep_Pz, 0)
     l_px = lep.Px()
     l_py = lep.Py()
     l_pz = lep.Pz()
@@ -27,7 +30,7 @@ def MakeNeutrino(lep_Px, lep_Py, lep_Pz, met, met_phi):
     l_E  = lep.E()
 
     mdiff = 0.5 * ( mW*mW - l_m*l_m )
-    pT_vl = v_px*l_px + v_py*l_py // pT( v * l )
+    pT_vl = v_px*l_px + v_py*l_py
 
     # Factors of the quadratic equation
     a = l_E*l_E - l_pz*l_pz
@@ -42,13 +45,14 @@ def MakeNeutrino(lep_Px, lep_Py, lep_Pz, met, met_phi):
     else:
         v_pz_1 = 0.5 * ( -b - np.sqrt(delta) ) / a
         v_pz_2 = 0.5 * ( -b + np.sqrt(delta) ) / a
-        if fabs(v_pz_1) < fabs(v_pz_2):
+        if np.abs(v_pz_1) < np.abs(v_pz_2):
             v_pz = v_pz_1
         else:
 	        v_pz = v_pz_2
 
     v_E  = np.sqrt( v_pT*v_pT + v_pz*v_pz )
 
+    m_neutrino = TLorentzVector()
     m_neutrino.SetPxPyPzE( v_px, v_py, v_pz, v_E )
 
     return m_neutrino
@@ -65,15 +69,12 @@ def ChiSquared(jets, mu, lep):
     t_had = W_had + jets[0]
     t_lep = W_lep + jets[1]
 
-    chi2 =  (W_had.M() - mW)**2 / sigmaW**2 +
-            (W_lep.M() - mW)**2 / sigmaW**2 +
-            (t_had.M() - mT)**2 / sigmaT**2 +
-            (t_lep.M() - mT)**2 / sigmaT**2
+    chi2 =  (W_had.M() - mW)**2 / sigmaW**2 + (W_lep.M() - mW)**2 / sigmaW**2 + (t_had.M() - mT)**2 / sigmaT**2 + (t_lep.M() - mT)**2 / sigmaT**2
 
     return chi2, [jets[0], jets[1], W_had, W_lep, t_had, t_lep]
 
 
-def reconstruct(jets_array, mu, lep):
+def reconstruct(jets, mu, lep):
     """Return (6 x 4) matrix with the following columns
     (b_had_px, b_lep_px, W_had_j1_px, W_had_j2_px)
     (b_had_py, b_lep_py, W_had_j1_py, W_had_j2_px)
@@ -128,14 +129,15 @@ def Predict(lep_arr, jet_arr):
     Calls helper functions in the following order:
     MakeNeutrino -> reconstruct -> FormatOutput
     """
-    lep = TLorentzVector().SetPxPyPzM(lep_arr[0], lep_arr[1], lep_arr[2], 0)
+    lep = TLorentzVector()
+    lep.SetPxPyPzE(lep_arr[0], lep_arr[1], lep_arr[2], np.sqrt(lep_arr[0]**2 + lep_arr[1]**2 + lep_arr[2]**2))
     # Skip the fourth element which is the lepton arival time of flight
     mu = MakeNeutrino(lep_arr[0], lep_arr[1], lep_arr[2], lep_arr[4], lep_arr[5])
 
     jets = []
     for i in range(len(jet_arr)):
-        j = TLorentzVector().SetPxPyPzE(jet_arr[i:0], jet_arr[i,1],
-                                        jet_arr[i,2], jet_arr[i,3])
+        j = TLorentzVector()
+        j.SetPxPyPzE(jet_arr[i,0], jet_arr[i,1],jet_arr[i,2], jet_arr[i,3])
         jets.append(j)
 
     best_combo, best_chi_squared = reconstruct(jets, mu, lep)
@@ -147,6 +149,7 @@ if __name__=="__main__":
     rep = "experimental"
 
     # Load Predictions and Scalars
+    print("Loading predictions and scalars")
     predictions = np.load('{}/predictions.npz'.format(training_dir))
     testing_input = predictions['input']
     true = predictions['true']
@@ -157,23 +160,29 @@ if __name__=="__main__":
         output_scalar = pickle.load(file_scaler)
 
     # Rescale the truth array
+    print("Rescaling inputs and outputs")
     true = true.reshape(true.shape[0], true.shape[1]*true.shape[2])
     true = output_scalar.inverse_transform(true)
-    true = true.reshape(true.shape[0], old_shape[0], old_shape[1])
+    true = true.reshape(true.shape[0], -1, 3)
 
     # UNDO Scaling
-    lep = lep_scalar.inverse_transform(testing_input[0])
-    jets = testing_input[1:,:-1]
-    jets = jets.reshape(-1, 30)
+    testing_input = testing_input.reshape(true.shape[0], 6, 6)
+    lep = lep_scalar.inverse_transform(testing_input[:,0])
+    jets = testing_input[:,1:,:-1]
+    jets = jets.reshape(-1, 25)
     jets = jets_scalar.inverse_transform(jets)
-    jets = jets.reshape(-1, 6, 5)
+    jets = jets.reshape(-1, 5, 5)
 
     # Predict each event
-    chi2_pred = np.zeros(jets.shape[0], 6, 3)
-    for i in range(len(jets.shape[0])):
+    print("Making predictions")
+    chi2_pred = np.zeros(shape=(jets.shape[0], 2, 3))
+    for i in range(jets.shape[0]):
         chi2_pred[i] += Predict(lep[i], jets[i])
+        if i < 10:
+            print(chi2_pred[i])
 
     # Save the predictions
+    print("Saving predictions")
     np.savez("%s/chi2pred" % training_dir, lep=lep, jets=jets, true=true, pred=chi2_pred)
 
     # INPUT BREAKDOWN
