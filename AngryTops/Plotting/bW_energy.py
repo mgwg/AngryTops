6a8f399b7dbe950b6577a9b620be6d82f211967d
Maggie Wang <mwang@huron.physics.utoronto.ca>
2021-06-03 17:44:48 -0400

    added correlations to energy plot

--- AngryTops/Plotting/bW_energy.py
+++ AngryTops/Plotting/bW_energy.py
@@ -59,20 +59,18 @@ histograms['Wb_had_E_true']        = TH1F( "Wb_had_E_true",   ";Hadronic W+b E [
 histograms['Wb_lep_E_true']        = TH1F( "Wb_lep_E_true",   ";Leptonic W+b E [GeV]", 50, 0., 500. )
 histograms['t_had_E_true']        = TH1F( "t_had_E_true",   ";Hadronic t E [GeV]", 50, 0., 500. )
 histograms['t_lep_E_true']        = TH1F( "t_lep_E_true",    ";Leptonic t E [GeV]", 50, 0., 500. )
-# Fitted
+# # Fitted
 histograms['Wb_had_E_fitted']        = TH1F( "Wb_had_E_fitted",   ";Hadronic W+b E [GeV]", 50, 0., 500. )
 histograms['Wb_lep_E_fitted']        = TH1F( "Wb_lep_E_fitted",   ";Leptonic W+b E [GeV]", 50, 0., 500. )
 histograms['t_had_E_fitted']        = TH1F( "t_had_E_fitted",   ";Hadronic t E [GeV]", 50, 0., 500. )
 histograms['t_lep_E_fitted']        = TH1F( "t_lep_E_fitted",    ";Leptonic t E [GeV]", 50, 0., 500. )
-
-################################################################################
-# FORMAT HISTOGRAMS
-# for hname, h in histograms.iteritems():
-#   h.Sumw2()
-#   if hname.endswith("true")>-1:
-#     h.SetMarkerColor(kRed)
-#     h.SetLineColor(kRed)
-#     h.SetMarkerStyle(24)
+# Correlations; t for true, p for predicted
+histograms['corr_tp_had_E']        = TH2F( "corr_tp_had_E",   ";True Hadronic W+b E [GeV];Predicted Hadronic W+b E [GeV]", 50, 150., 500., 50, 150., 500. )
+histograms['corr_tp_lep_E']        = TH2F( "corr_tp_lep_E",   ";True Leptonic W+b E [GeV];Predicted Leptonic W+b E [GeV]", 50, 150., 500., 50, 150., 500. )
+histograms['corr_pp_had_E']        = TH2F( "corr_pp_had_E",   ";Predicted Hadronic t E [GeV];Predicted Hadronic W+b E [GeV]", 50, 150., 500., 50, 150., 500. )
+histograms['corr_pp_lep_E']        = TH2F( "corr_pp_lep_E",    ";Predicted Leptonic t E [GeV];Predicted Leptonic W+b E [GeV]", 50, 150., 500., 50, 150., 500. )
+histograms['corr_tt_had_E']        = TH2F( "corr_tt_had_E",   ";True Hadronic t E [GeV];True Hadronic W+b E [GeV]", 50, 150., 500., 50, 150., 500. )
+histograms['corr_tt_lep_E']        = TH2F( "corr_tt_lep_E",    ";True Leptonic t E [GeV];True Leptonic W+b E [GeV]", 50, 150., 500., 50, 150., 500. )
 
 ################################################################################
 # POPULATE HISTOGRAMS
@@ -126,6 +124,13 @@ for i in range(n_events):
     histograms['t_lep_E_true'].Fill( t_lep_true.E(), w)
     histograms['t_had_E_fitted'].Fill( t_had_fitted.E(), w)
     histograms['t_lep_E_fitted'].Fill( t_lep_fitted.E(), w)
+  
+    histograms['corr_tp_had_E'].Fill( Wb_had_E_true, Wb_had_E_fitted, w )  
+    histograms['corr_tp_lep_E'].Fill( Wb_lep_E_true, Wb_lep_E_fitted, w )
+    histograms['corr_pp_had_E'].Fill( t_had_fitted.E(), Wb_had_E_fitted, w )  
+    histograms['corr_pp_lep_E'].Fill( t_lep_fitted.E(), Wb_lep_E_fitted, w )  
+    histograms['corr_tt_had_E'].Fill( t_had_true.E(), Wb_had_E_true, w )
+    histograms['corr_tt_lep_E'].Fill( t_lep_true.E(), Wb_lep_E_true, w )
 
     n_good += 1
 
@@ -136,7 +141,6 @@ ofile.Write()
 ofile.Close()
 
 print("Finished. Saved output file:", ofilename)
-
 f_good = 100. * float( n_good ) / float( n_events )
 print("Good events: %.2f" % f_good)
 
@@ -144,23 +148,16 @@ print("Good events: %.2f" % f_good)
 gStyle.SetPalette(kGreyScale)
 gROOT.GetColor(52).InvertPalette()
 
-def plot_observables(obs):
+def plot_observables(fitted, true):
     # Load the histograms
-    hname_true = "Wb_%s_true" % (obs)
-    hame_fitted = "Wb_%s_fitted" % (obs)
+    hname_true = true
+    hame_fitted = fitted
 
     # True and fitted leaf
     h_true = infile_plot.Get(hname_true)
     h_fitted = infile_plot.Get(hame_fitted)
     if h_true == None:
         print ("ERROR: invalid histogram for", obs)
-
-    # t graphs
-    t_name_true = "t_%s_true" % (obs)
-    t_name_fitted = "t_%s_fitted" % (obs)
-    t_true = infile_plot.Get(t_name_true)
-    t_fitted = infile_plot.Get(t_name_fitted)
-    print(t_name_true, t_name_fitted)
 
     # Axis titles
     xtitle = h_true.GetXaxis().GetTitle()
@@ -171,15 +168,10 @@ def plot_observables(obs):
     else:
         Normalize(h_true)
         Normalize(h_fitted)
-        Normalize(t_true)
-        Normalize(t_fitted)
 
     # Set Style
     SetTH1FStyle( h_true,  color=kGray+2, fillstyle=1001, fillcolor=kGray, linewidth=3, markersize=0 )
     SetTH1FStyle( h_fitted, color=kBlack, markersize=0, markerstyle=20, linewidth=3 )
-    # t graph
-    SetTH1FStyle( t_true, color=46, markersize=0, markerstyle=20, linewidth=3 ) # red
-    SetTH1FStyle( t_fitted, color=38, markersize=0, markerstyle=20, linewidth=3 ) # blue
 
     c, pad0, pad1 = MakeCanvas()
     pad0.cd()
@@ -187,31 +179,47 @@ def plot_observables(obs):
 
     h_true.Draw("h")
     h_fitted.Draw("h same")
-    t_true.Draw("h same")
-    t_fitted.Draw("h same")
     hmax = 1.5 * max( [ h_true.GetMaximum(), h_fitted.GetMaximum() ] )
     h_fitted.SetMaximum( hmax )
     h_true.SetMaximum( hmax )
     h_fitted.SetMinimum( 0. )
     h_true.SetMinimum( 0. )
-    # t graph
-    t_fitted.SetMaximum( hmax )
-    t_true.SetMaximum( hmax )
-    t_fitted.SetMinimum( 0. )
-    t_true.SetMinimum( 0. )
+
+    # set legend labels
+    if "_fitted" in fitted and "_true" in true:
+        leg_true = "MG5+Py8 W+b"
+        leg_fitted = "Predicted W+b"
+    elif "_fitted" in fitted and "_fitted" in true: 
+        leg_true = "Predicted t"
+        leg_fitted = "Predicted W+b"
+    else: # plot truth W+b against truth t
+        leg_true = "MG5+Py8 t"
+        leg_fitted = "MG5+Py8 W+b"
 
     leg = TLegend( 0.20, 0.80, 0.50, 0.90 )
     leg.SetFillColor(0)
     leg.SetFillStyle(0)
     leg.SetBorderSize(0)
     leg.SetTextFont(42)
-    leg.SetTextSize(0.03)
-    leg.AddEntry( h_true, "MG5+Py8 W + b", "f" )
-    leg.AddEntry( h_fitted, "Predicted W + b", "f" )
-    leg.AddEntry( t_true, "MG5+Py8 t", "f" )
-    leg.AddEntry( t_fitted, "Predicted t", "f" )
+    leg.SetTextSize(0.05)
+    leg.AddEntry( h_true, leg_true, "f" )
+    leg.AddEntry( h_fitted, leg_fitted, "f" )
     leg.SetY1( leg.GetY1() - 0.05 * leg.GetNRows() )
     leg.Draw()
+
+    KS = h_true.KolmogorovTest( h_fitted )
+    X2 = ChiSquared(h_true, h_fitted) # UU NORM
+    
+    # get mean and standard deviation
+    h_true.GetMean() #axis=1 by default for x-axis
+    h_true.GetStdDev()
+
+    l = TLatex()
+    l.SetNDC()
+    l.SetTextFont(42)
+    l.SetTextColor(kBlack)
+    l.DrawLatex( 0.7, 0.80, "KS test: %.2f" % KS )
+    l.DrawLatex( 0.7, 0.75, "#chi^{2}/NDF = %.2f" % X2 )
 
     gPad.RedrawAxis()
     if caption is not None:
@@ -237,9 +245,59 @@ def plot_observables(obs):
 
     c.cd()
 
-    c.SaveAs("{0}/E_fit/{1}.png".format(training_dir, obs))
+    c.SaveAs("{0}/E_fit/{1}_{2}.png".format(training_dir, fitted, true))
     pad0.Close()
     pad1.Close()
+    c.Close()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def plot_correlations(hist_name):
+
+    # True and fitted leaf
+    hist = infile_plot.Get(hist_name)
+    if hist == None:
+        print ("ERROR: invalid histogram for", hist_name)
+
+    SetTH1FStyle(hist,  color=kGray+2, fillstyle=6)
+
+    c = TCanvas()
+    c.cd()
+
+    pad0 = TPad( "pad0","pad0",0, 0,1,1,0,0,0 )
+    pad0.SetLeftMargin( 0.18 )
+    pad0.SetRightMargin( 0.05 )
+    pad0.SetBottomMargin( 0.18 )
+    pad0.SetTopMargin( 0.07 )
+    pad0.SetFillColor(0)
+    pad0.SetFillStyle(4000)
+    pad0.Draw()
+    pad0.cd()
+
+    hist.Draw("colz")
+
+    corr = hist.GetCorrelationFactor()
+    l = TLatex()
+    l.SetNDC()
+    l.SetTextFont(42)
+    l.SetTextColor(kBlack)
+    l.DrawLatex( 0.2, 0.8, "Corr Coeff: %.2f" % corr )
+
+    gPad.RedrawAxis()
+
+    if caption is not None:
+        newpad = TPad("newpad","a caption",0.1,0,1,1)
+        newpad.SetFillStyle(4000)
+        newpad.Draw()
+        newpad.cd()
+        title = TPaveLabel(0.1,0.94,0.9,0.99,caption)
+        title.SetFillColor(16)
+        title.SetTextFont(52)
+        title.Draw()
+
+    c.cd()
+
+    c.SaveAs("{0}/E_fit/{1}.png".format(training_dir, hist_name))
+    pad0.Close()
     c.Close()
 
 ################################################################################
@@ -251,8 +309,14 @@ if __name__==   "__main__":
     infilename_plot = "{}/histograms_W_plus_b.root".format(training_dir)
     infile_plot = TFile.Open(infilename_plot)
 
-    attributes = ['had_E', 'lep_E']
+    plot_observables('Wb_had_E_fitted', 'Wb_had_E_true')
+    plot_observables('Wb_lep_E_fitted', 'Wb_lep_E_true')
+    plot_observables('Wb_had_E_fitted', 't_had_E_fitted')
+    plot_observables('Wb_lep_E_fitted', 't_lep_E_fitted')
+    plot_observables('Wb_had_E_true', 't_had_E_true')
+    plot_observables('Wb_lep_E_true', 't_lep_E_true')    
 
-    # Make a plot for each observable
-    for obs in attributes:
-        plot_observables(obs)
+    # Draw 2D Correlations
+    corr_2d = ["corr_tp_had_E", "corr_tp_lep_E", "corr_pp_had_E", "corr_pp_lep_E", "corr_tt_had_E", "corr_tt_lep_E"]
+    for corr in corr_2d:
+        plot_correlations(corr)
