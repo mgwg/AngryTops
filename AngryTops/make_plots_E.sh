f389ad00ddbdfdd741c14324c0994574e7acf87d
mgwg <wangmaggief@gmail.com>
2021-06-04 13:38:55 -0400

    ahhhh

--- AngryTops/make_plots_E.sh
+++ AngryTops/make_plots_E.sh
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
-python2 Plotting/bW_energy.py $1 $2 $3
-=======
-#!/bin/bash
-python2 Plotting/bW_energy.py $1 $2 $3
->>>>>>> 1bf561fb4cd46f312deecb7db9d058ad6bfa0f45
+python2 Plotting/bW_energy.py $1 $2 $3 $4
+
