39ae91e37d9f61d0e88353b7fe50a34467cc43f5
mgwg <wangmaggief@gmail.com>
2021-11-06 11:35:55 -0400

    a

--- AngryTops/Plotting/jet_filter.py
+++ AngryTops/Plotting/jet_filter.py
@@ -7,7 +7,7 @@ from AngryTops.features import *
 
 ################################################################################
 # CONSTANTS
-training_dir = sys.argv[1]
+infilename = "../May21/predictions_May21.root"
 output_dir = sys.argv[2]
 representation = sys.argv[3]
 scaling = True              # whether the dataset has been passed through a scaling function or not
@@ -16,82 +16,14 @@ m_t = 172.5
 m_W = 80.4
 m_b = 4.95
 
-ALL = 0
-NONE = 1
-ONLY = 2
 b_tagging = NONE   
 
-################################################################################
-# load data
-
-print("INFO: fitting ttbar decay chain...")
-predictions = np.load(training_dir + 'predictions.npz')
-jets = predictions['input']
-true = predictions['true']
-
-particles_shape = (true.shape[1], true.shape[2])
-print("jets shape", jets.shape)
-print("b tagging option", b_tagging)
-if scaling:
-    scaler_filename = training_dir + "scalers.pkl"
-    with open( scaler_filename, "rb" ) as file_scaler:
-        jets_scalar = pickle.load(file_scaler)
-        lep_scalar = pickle.load(file_scaler)
-        output_scalar = pickle.load(file_scaler)
-        # Rescale the truth array
-        true = true.reshape(true.shape[0], true.shape[1]*true.shape[2])
-        true = output_scalar.inverse_transform(true)
-        true = true.reshape(true.shape[0], particles_shape[0], particles_shape[1])
-        # Rescale the jets array
-        jets_lep = jets[:,:6]
-        jets_jets = jets[:,6:] # remove muon column
-        jets_jets = jets_jets.reshape((jets_jets.shape[0],5,6)) # reshape to 5 x 6 array
-
-        # Remove the b-tagging states and put them into a new array to be re-appended later.
-        b_tags = jets_jets[:,:,5]
-        jets_jets = np.delete(jets_jets, 5, 2) # delete the b-tagging states
-
-        jets_jets = jets_jets.reshape((jets_jets.shape[0], 25)) # reshape into 25 element long array
-        jets_lep = lep_scalar.inverse_transform(jets_lep)
-        jets_jets = jets_scalar.inverse_transform(jets_jets) # scale values ... ?
-        #I think this is the final 6x6 array the arxiv paper was talking about - 5 x 5 array containing jets (1 per row) and corresponding px, py, pz, E, m
-        jets_jets = jets_jets.reshape((jets_jets.shape[0],5,5))
-        # Re-append the b-tagging states as a column at the end of jets_jets 
-        jets_jets = np.append(jets_jets, np.expand_dims(b_tags, 2), 2)
-
-if not scaling:
-    jets_lep = jets[:,:6]
-    jets_jets = jets[:,6:]
-    jets_jets = jets_jets.reshape((jets_jets.shape[0],5,6))
-    jets_jets = np.delete(jets_jets, 5, 2)
-
-# jets
-jet_mu = jets_lep
-# First jet for every event
-jet_1 = jets_jets[:,0]
-# Second jet for every event
-jet_2 = jets_jets[:,1]
-jet_3 = jets_jets[:,2]
-jet_4 = jets_jets[:,3]
-jet_5 = jets_jets[:,4]
-# Create an array with each jet's arrays for accessing b-tagging states later.
-jet_list = np.stack([jet_1, jet_2, jet_3, jet_4, jet_5]) 
-
-# truth
-y_true_W_had = true[:,0,:]
-y_true_W_lep = true[:,1,:]
-y_true_b_had = true[:,2,:]
-y_true_b_lep = true[:,3,:]
-y_true_t_had = true[:,4,:]
-y_true_t_lep = true[:,5,:]
-
-# store number of events as a separate variable for clarity
-n_events = true.shape[0]
-w = 1
-print("INFO ...done")
+# t = TFile.Open(infilename)
+infile = Tfile(infilename, "READ")
+t = infile.Get("nominal")
 
 ################################################################################
-# MAKE ROOT FILE
+# MAKE ROOT FILE FOR HISTS
 
 ofilename = "{}/jet_filter_hists".format(output_dir)
 # Open output file
@@ -99,73 +31,73 @@ ofile = TFile.Open( ofilename, "recreate" )
 ofile.cd()
 
 ################################################################################
-# HISTOGRAMS
-hists = {}
-hists['had_W_Pt'] = TH1F("had_W_Pt","p_{T} (GeV)", 50, 0, 500)
-# hists['jet12_had_W_Pt_diff'] = TH1F("jet12_had_W_Pt_diff", "p_{T} (GeV)", 50, -300, 300)
-# hists['jet12_Pt'] = TH1F("jet12_Pt", "p_{T} (GeV)", 50, 0, 500)
-
-for i in range(5):
-    hists['max_jet_{}_Pt'.format(i)] = TH1F("max_jet_{}_Pt'.format(i)","p_{T} (GeV)", 50, 0, 500)
-    hists['jet_{}_had_W_Pt_diff'.format(i)] = TH1F("jet_{}_had_W_Pt_diff".format(i),"p_{T} (GeV)", 50, -300, 300)
-    hists['jet_{}_had_W_Pt_diff'.format(i)].SetTitle("Leading Jet p_{T} - Had W p_{T}; p_{T} (GeV);A.U.")
-
-################################################################################
 # POPULATE TREE
 
 print("INFO: starting event loop. Found %i events" % n_events)
 
-# px = t.AsMatrix(["var1"])
-# py = t.AsMatrix(["var1"])
-# pz = t.AsMatrix(["var1"])
+jet1pt = t.AsMatrix(["b_jet1_pt_obs"])
+jet2pt = t.AsMatrix(["b_jet2_pt_obs"])
+jet3pt = t.AsMatrix(["b_jet3_pt_obs"])
+jet4pt = t.AsMatrix(["b_jet4_pt_obs"])
+jet5pt = t.AsMatrix(["b_jet5_pt_obs"])
+jet1btag = t.AsMatrix(["b_jet1_btag_obs"])
+jet2btag = t.AsMatrix(["b_jet2_btag_obs"])
+jet3btag = t.AsMatrix(["b_jet3_btag_obs"])
+jet4btag = t.AsMatrix(["b_jet4_btag_obs"])
+jet5btag = t.AsMatrix(["b_jet5_btag_obs"])
+hadWpt = t.AsMatrix(["b_W_had_pt_true"])
 
-# Print out example
-for i in range(n_events):
-    if ( n_events < 10 ) or ( (i+1) % int(float(n_events)/10.)  == 0 ):
-        perc = 100. * i / float(n_events)
-        print("INFO: Event %-9i  (%3.0f %%)" % ( i, perc ))
+n_events = t.GetEntries()
 
-    # make jets
-        
-    W_had_true   = MakeP4( y_true_W_had[i], m_W, representation)
-    W_lep_true   = MakeP4( y_true_W_lep[i], m_W , representation)
-    b_had_true   = MakeP4( y_true_b_had[i], m_b , representation)
-    b_lep_true   = MakeP4( y_true_b_lep[i], m_b , representation)
-    t_had_true   = MakeP4( y_true_t_had[i], m_t , representation)
-    t_lep_true   = MakeP4( y_true_t_lep[i], m_t , representation)
+################################################################################
+# HISTOGRAMS
+hists = {}
+t.Draw("b_W_had_pt_true >> had_W_Pt(50,0,500)")
+t.Draw("b_b_had_pt_true >> had_b_Pt(50,0,500)")
+t.Draw("b_b_lep_pt_true >> lep_W_Pt(50,0,500)")
+had_W_Pt = gROOT.FindObject ("had_W_Pt")
+had_b_Pt = gROOT.FindObject ("had_b_Pt")
+lep_b_Pt = gROOT.FindObject ("lep_b_Pt")
+hists['had_W_Pt'] = htemp.Clone("had_W_Pt")
+hists['had_b_Pt'] = htemp.Clone("had_b_Pt")
+hists['lep_b_Pt'] = htemp.Clone("lep_b_Pt")
 
-    jet_mu_vect = MakeP4(jet_mu[i],jet_mu[i][4], representation)
-
-    jet_1_vect = MakeP4(jet_1[i], jet_1[i][4], representation)
-    jet_2_vect = MakeP4(jet_2[i], jet_2[i][4], representation)
-    jet_3_vect = MakeP4(jet_3[i], jet_3[i][4], representation)
-    jet_4_vect = MakeP4(jet_4[i], jet_4[i][4], representation)
-    jet_5_vect = MakeP4(jet_5[i], jet_5[i][4], representation)
-
+for i in range(5):
+    hists['max_jet_{}_Pt'.format(i)] = TH1F("max_jet_{}_Pt'.format(i)","p_{T} (GeV)", 50, 0, 500)
+    hists['max_jet_{}_Pt'.format(i)].SetTitle("{} leading jet pT".format(i+1))
+    hists['jet_{}_had_W_Pt_diff'.format(i)] = TH1F("jet_{}_had_W_Pt_diff".format(i),"p_{T} (GeV)", 50, -300, 300)
+    hists['jet_{}_had_W_Pt_diff'.format(i)].SetTitle("Leading Jet p_{T} - Had W p_{T}; p_{T} (GeV);A.U.")
     
-    # # add list containing jets of correspoonding event
-    # jets = [jet_1_vect, jet_2_vect, jet_3_vect, jet_4_vect]
-    # jets_Pt = [jet_1_vect.Pt(), jet_2_vect.Pt(), jet_3_vect.Pt(), jet_4_vect.Pt()]
-    # # sort in decreasing order
-    # jets_Pt.sort(reverse = True)
+    # b tagged jets
+    hists['b_max_jet_{}_Pt'.format(i)] = TH1F("max_jet_{}_Pt'.format(i)","p_{T} (GeV)", 50, 0, 500)
+    hists['b_max_jet_{}_Pt'.format(i)].SetTitle("{} leading jet pT".format(i+1))
 
-    # # If there is no fifth jet, do not append it to list of jets to avoid counting it
-    # if not np.all(jet_5[i] == 0.):
-    #     jets.append(jet_5_vect)
-    #     jets_Pt.append(jet_5_vect.Pt())
+# each row corresponds to an event
+jets_Pt = np.stack([jet1pt, jet2pt, jet3pt, jet4pt, jet5pt], axis = 1)
+# btagging mask
+jets_btag = np.stack([jet1btag, jet2btag, jet3btag, jet4btag, jet5btag], axis = 1)
 
-    # # filter b-tagged jets
-    # for j in range(len(jets)):
-    #     # only considering non b tagged jets
-    #     if (not jet_list[m, i, 5]):
-    #         good_jets.remove(jets[m])
+# only keep jets that are NOT btagged
+jets_notb_Pt[jets_btag == 0] = 0
+# remove events where all jets are btagged
+jets_notb_Pt[~np.all(jets_notb_Pt==0, axis =1)]
 
-    # hists['had_W_Pt'].Fill(W_had_true.Pt())
-    # for i in range(len(jets_Pt)):
-    #     hists['max_jet_{}_Pt'.format(i)].Fill(jets_Pt[i])
-    #     hists['jet_{}_had_W_Pt_diff'.format(i)].Fill( jets_Pt[i]-W_had_true.Pt() )
-    # # hists['jet12_had_W_Pt_diff'].Fill(jets_Pt[0]+jets_Pt[-1]-W_had_true.Pt())
-    # # hists['jet12_Pt'].Fill(jets_Pt[0]+jets_Pt[-1])
+# find btagged jets
+jets_b_Pt[jets_btag != 0] = 0
+jets_b_Pt[~np.all(jets_b_Pt==0, axis =1)]
+
+# sort jets from smallest to largest
+jets_notb_Pt = np.sort(jets_notb_Pt)
+jets_b_Pt = np.sort(jets_b_Pt)
+# reverse array order to list jets from largest to smallest to match order of leading jet (i.e. 0th is the largest, etc.)
+jets_b_Pt = np.flip(jets_b_Pt, axis = 1)
+
+for i in range(5): 
+    jets_Pt_max = jets_Pt[:,i] 
+    hists['max_jet_{}_Pt'.format(i)].FillN(len(jets_Pt_max), jets_Pt_max)
+    hists['jet_{}_had_W_Pt_diff'.format(i)].FillN(len(jets_Pt_max), jets_Pt_max - hadWpt)
+
+    hists['b_max_jet_{}_Pt'.format(i)].FillN(len(jets_Pt_max), jets_Pt_max)
 
 for histname in hists:
     hists[histname].Write(histname)
@@ -324,31 +256,24 @@ def Normalize( h, sf=1.0 ):
   h.Scale( sf / A )
 
 hist_W = infile.Get('had_W_Pt')
-hist_jet12_W_pt_diff = infile.Get('jet12_had_W_Pt_diff')
-plot_hist(hist_jet12_W_pt_diff, 'jet12_had_W_Pt_diff', "Leading Jet 1 + Jet 3 p_{T} - Hadronic W p_{T}")
-gStyle.SetOptStat("emr");
+hist_b_had = infile.Get('had_b_Pt')
+hist_b_lep = infile.Get('lep_b_Pt')
+
+gStyle.SetOptStat("emr")#;
 for i in range(5):
     hist_pt_diff = infile.Get('jet_{}_had_W_Pt_diff'.format(i))
-    hist_jet = infile.Get('max_jet_{}_Pt'.format(i))
-    hist_jet.SetTitle("{} leading jet pT".format(i+1))
-    Normalize(hist_jet)
-    plot_hist(hist_jet, 'jet_{}_Pt'.format(i), str(i+1)+' Leading Jet p_{T}')
     plot_hist(hist_pt_diff, 'jet_{}_W_Pt_diff'.format(i), str(i+1)+' Leading Jet p_{T} - Hadronic W p_{T}')
 
-
-# infilename_truth = "{}/histograms.root".format("../CheckPoints/May27")
-# infile_truth = TFile.Open(infilename_truth)
-
-# # Make a plot for each observable
-# for obs in attributes:
-#     if "_pt" in obs:
-#         plot_hist(obs)
-
-
+# plot in a separate loop because the style changes and I haven't figured out how to reverse it yet
 from AngryTops.Plotting.PlottingHelper import *
-
-hist_jet12_W = infile.Get('jet12_Pt')
-plot_observables(hist_jet12_W, hist_W, 'jet12_v_W_Pt', "Leading Jet 1 + Jet 3 p_{T} vs Hadronic W p_{T}")
+Normalize(hist_W)
+Normalize(hist_b_had)
+Normalize(hist_b_lep)
 for i in range(5):
     hist_jet = infile.Get('max_jet_{}_Pt'.format(i))
+    hist_jet_b = infile.Get('b_max_jet_{}_Pt'.format(i))
+    Normalize(hist_jet)
+    Normalize(hist_jet_b)
     plot_observables(hist_jet, hist_W, 'jet_{}_v_W_Pt'.format(i), str(i+1)+' Leading Jet p_{T} vs Hadronic W p_{T}')
+    plot_observables(hist_jet, hist_b_had, 'jet_{}_v_had_b_Pt'.format(i), str(i+1)+' Leading Jet p_{T} vs Hadronic b p_{T}')
+    plot_observables(hist_jet, hist_b_lep, 'jet_{}_v_lep_b_Pt'.format(i), str(i+1)+' Leading Jet p_{T} vs Leptonic b p_{T}')
--- AngryTops/Plotting/rough.py
+++ AngryTops/Plotting/rough.py
@@ -2,8 +2,9 @@
 from ROOT import *
 import numpy as np
 from AngryTops.features import *
-
+import array
 import pickle
+from AngryTops.Plotting.identification_helper import MakeP4
 
 # write to tree file
 
@@ -30,6 +31,7 @@ print("INFO: fitting ttbar decay chain...")
 predictions = np.load(training_dir + 'predictions.npz')
 jets = predictions['input']
 true = predictions['true']
+fitted = predictions['pred']
 
 particles_shape = (true.shape[1], true.shape[2])
 print("jets shape", jets.shape)
@@ -44,6 +46,10 @@ if scaling:
         true = true.reshape(true.shape[0], true.shape[1]*true.shape[2])
         true = output_scalar.inverse_transform(true)
         true = true.reshape(true.shape[0], particles_shape[0], particles_shape[1])
+        # Rescale the fitted array
+        fitted = fitted.reshape(fitted.shape[0], fitted.shape[1]*fitted.shape[2])
+        fitted = output_scalar.inverse_transform(fitted)
+        fitted = fitted.reshape(fitted.shape[0], particles_shape[0], particles_shape[1])
         # Rescale the jets array
         jets_lep = jets[:,:6]
         jets_jets = jets[:,6:] # remove muon column
@@ -87,6 +93,14 @@ y_true_b_lep = true[:,3,:]
 y_true_t_had = true[:,4,:]
 y_true_t_lep = true[:,5,:]
 
+# fitted
+y_fitted_W_had = fitted[:,0,:]
+y_fitted_W_lep = fitted[:,1,:]
+y_fitted_b_had = fitted[:,2,:]
+y_fitted_b_lep = fitted[:,3,:]
+y_fitted_t_had = fitted[:,4,:]
+y_fitted_t_lep = fitted[:,5,:]
+
 # store number of events as a separate variable for clarity
 n_events = true.shape[0]
 w = 1
@@ -100,30 +114,35 @@ ofile.cd()
 b_jet1_px_obs   = array.array( 'f', [ -1.] )
 b_jet1_py_obs   = array.array( 'f', [ -1.] )
 b_jet1_pz_obs   = array.array( 'f', [ -1.] )
+b_jet1_pt_obs   = array.array( 'f', [ -1.] )
 b_jet1_E_obs    = array.array( 'f', [ -1.] )
 b_jet1_m_obs    = array.array( 'f', [ -1.] )
 b_jet1_btag_obs   = array.array( 'f', [ -1.] )
 b_jet2_px_obs   = array.array( 'f', [ -1.] )
 b_jet2_py_obs   = array.array( 'f', [ -1.] )
 b_jet2_pz_obs   = array.array( 'f', [ -1.] )
+b_jet2_pt_obs   = array.array( 'f', [ -1.] )
 b_jet2_E_obs    = array.array( 'f', [ -1.] )
 b_jet2_m_obs    = array.array( 'f', [ -1.] )
 b_jet2_btag_obs   = array.array( 'f', [ -1.] )
 b_jet3_px_obs   = array.array( 'f', [ -1.] )
 b_jet3_py_obs   = array.array( 'f', [ -1.] )
 b_jet3_pz_obs   = array.array( 'f', [ -1.] )
+b_jet3_pt_obs   = array.array( 'f', [ -1.] )
 b_jet3_E_obs    = array.array( 'f', [ -1.] )
 b_jet3_m_obs    = array.array( 'f', [ -1.] )
 b_jet3_btag_obs   = array.array( 'f', [ -1.] )
 b_jet4_px_obs   = array.array( 'f', [ -1.] )
 b_jet4_py_obs   = array.array( 'f', [ -1.] )
 b_jet4_pz_obs   = array.array( 'f', [ -1.] )
+b_jet4_pt_obs   = array.array( 'f', [ -1.] )
 b_jet4_E_obs    = array.array( 'f', [ -1.] )
 b_jet4_m_obs    = array.array( 'f', [ -1.] )
 b_jet4_btag_obs   = array.array( 'f', [ -1.] )
 b_jet5_px_obs   = array.array( 'f', [ -1.] )
 b_jet5_py_obs   = array.array( 'f', [ -1.] )
 b_jet5_pz_obs   = array.array( 'f', [ -1.] )
+b_jet5_pt_obs   = array.array( 'f', [ -1.] )
 b_jet5_E_obs    = array.array( 'f', [ -1.] )
 b_jet5_m_obs    = array.array( 'f', [ -1.] )
 b_jet5_btag_obs   = array.array( 'f', [ -1.] )
@@ -232,42 +251,46 @@ b_t_lep_pt_fitted   = array.array( 'f', [ -1.] )
 b_t_lep_y_fitted    = array.array( 'f', [ -1.] )
 b_t_lep_phi_fitted  = array.array( 'f', [ -1.] )
 
+b_runNumber  = array.array( 'i', [ -1] )
+
 tree = TTree( "nominal", "nominal" )
-tree.Branch( "eventNumber",     b_eventNumber,     'eventNumber/l' )
 tree.Branch( 'runNumber',       b_runNumber,       'runNumber/i' )
-tree.Branch( 'mcChannelNumber', b_mcChannelNumber, 'mcChannelNumber/i' )
-tree.Branch( 'weight_mc',       b_weight_mc,       'weight_mc/F' )
 
 tree.Branch( 'b_jet1_px_obs', b_jet1_px_obs, 'b_jet1_px_obs/F')
 tree.Branch( 'b_jet1_py_obs', b_jet1_py_obs, 'b_jet1_py_obs/F')
-tree.Branch( 'b_jet1_pz_obs', b_jet1_pz_obs, 'b_jet1_py_obs/F')
+tree.Branch( 'b_jet1_pz_obs', b_jet1_pz_obs, 'b_jet1_pz_obs/F')
+tree.Branch( 'b_jet1_pt_obs', b_jet1_pt_obs, 'b_jet1_pt_obs/F')
 tree.Branch( 'b_jet1_E_obs', b_jet1_E_obs, 'b_jet1_E_obs/F')
 tree.Branch( 'b_jet1_m_obs', b_jet1_m_obs, 'b_jet1_m_obs/F')
-tree.Branch( 'b_jet1_btag_obs', b_jet1_pt_obs, 'b_jet1_btag_obs/F')
+tree.Branch( 'b_jet1_btag_obs', b_jet1_btag_obs, 'b_jet1_btag_obs/F')
 tree.Branch( 'b_jet2_px_obs', b_jet2_px_obs, 'b_jet2_px_obs/F')
 tree.Branch( 'b_jet2_py_obs', b_jet2_py_obs, 'b_jet2_py_obs/F')
-tree.Branch( 'b_jet2_pz_obs', b_jet2_pz_obs, 'b_jet2_py_obs/F')
+tree.Branch( 'b_jet2_pz_obs', b_jet2_pz_obs, 'b_jet2_pz_obs/F')
+tree.Branch( 'b_jet2_pt_obs', b_jet2_pt_obs, 'b_jet2_pt_obs/F')
 tree.Branch( 'b_jet2_E_obs', b_jet2_E_obs, 'b_jet2_E_obs/F')
 tree.Branch( 'b_jet2_m_obs', b_jet2_m_obs, 'b_jet2_m_obs/F')
-tree.Branch( 'b_jet2_btag_obs', b_jet2_pt_obs, 'b_jet2_btag_obs/F')
+tree.Branch( 'b_jet2_btag_obs', b_jet2_btag_obs, 'b_jet2_btag_obs/F')
 tree.Branch( 'b_jet3_px_obs', b_jet3_px_obs, 'b_jet3_px_obs/F')
 tree.Branch( 'b_jet3_py_obs', b_jet3_py_obs, 'b_jet3_py_obs/F')
-tree.Branch( 'b_jet3_pz_obs', b_jet3_pz_obs, 'b_jet3_py_obs/F')
+tree.Branch( 'b_jet3_pz_obs', b_jet3_pz_obs, 'b_jet3_pz_obs/F')
+tree.Branch( 'b_jet3_pt_obs', b_jet3_pt_obs, 'b_jet3_pt_obs/F')
 tree.Branch( 'b_jet3_E_obs', b_jet3_E_obs, 'b_jet3_E_obs/F')
 tree.Branch( 'b_jet3_m_obs', b_jet3_m_obs, 'b_jet3_m_obs/F')
-tree.Branch( 'b_jet3_btag_obs', b_jet3_pt_obs, 'b_jet3_btag_obs/F')
+tree.Branch( 'b_jet3_btag_obs', b_jet3_btag_obs, 'b_jet3_btag_obs/F')
 tree.Branch( 'b_jet4_px_obs', b_jet4_px_obs, 'b_jet4_px_obs/F')
 tree.Branch( 'b_jet4_py_obs', b_jet4_py_obs, 'b_jet4_py_obs/F')
-tree.Branch( 'b_jet4_pz_obs', b_jet4_pz_obs, 'b_jet4_py_obs/F')
+tree.Branch( 'b_jet4_pz_obs', b_jet4_pz_obs, 'b_jet4_pz_obs/F')
+tree.Branch( 'b_jet4_pt_obs', b_jet4_pt_obs, 'b_jet4_pt_obs/F')
 tree.Branch( 'b_jet4_E_obs', b_jet4_E_obs, 'b_jet4_E_obs/F')
 tree.Branch( 'b_jet4_m_obs', b_jet4_m_obs, 'b_jet4_m_obs/F')
-tree.Branch( 'b_jet4_btag_obs', b_jet4_pt_obs, 'b_jet4_btag_obs/F')
+tree.Branch( 'b_jet4_btag_obs', b_jet4_btag_obs, 'b_jet4_btag_obs/F')
 tree.Branch( 'b_jet5_px_obs', b_jet5_px_obs, 'b_jet5_px_obs/F')
 tree.Branch( 'b_jet5_py_obs', b_jet5_py_obs, 'b_jet5_py_obs/F')
-tree.Branch( 'b_jet5_pz_obs', b_jet5_pz_obs, 'b_jet5_py_obs/F')
+tree.Branch( 'b_jet5_pz_obs', b_jet5_pz_obs, 'b_jet5_pz_obs/F')
+tree.Branch( 'b_jet5_pt_obs', b_jet5_pt_obs, 'b_jet5_pt_obs/F')
 tree.Branch( 'b_jet5_E_obs', b_jet5_E_obs, 'b_jet5_E_obs/F')
 tree.Branch( 'b_jet5_m_obs', b_jet5_m_obs, 'b_jet5_m_obs/F')
-tree.Branch( 'b_jet5_btag_obs', b_jet5_pt_obs, 'b_jet5_btag_obs/F')
+tree.Branch( 'b_jet5_btag_obs', b_jet5_btag_obs, 'b_jet5_btag_obs/F')
 tree.Branch( 'b_jetmu_px_obs', b_jetmu_px_obs, 'b_jetmu_px_obs/F')
 tree.Branch( 'b_jetmu_py_obs', b_jetmu_py_obs, 'b_jetmu_py_obs/F')
 tree.Branch( 'b_jetmu_pz_obs', b_jetmu_pz_obs, 'b_jetmu_py_obs/F')
@@ -378,11 +401,8 @@ for i in range(n_events):
         perc = 100. * i / float(n_events)
         print("INFO: Event %-9i  (%3.0f %%)" % ( i, perc ))
 
-    w = 1
-    jets_n  = event_info[i][3]
-    bjets_n = event_info[i][4]
+    
 
-        
     W_had_true   = MakeP4( y_true_W_had[i], m_W, representation)
     W_had_fitted = MakeP4( y_fitted_W_had[i],  m_W, representation)
 
@@ -407,43 +427,49 @@ for i in range(n_events):
     jet_4_vect = MakeP4(jet_4[i], jet_4[i][4], representation)
     jet_5_vect = MakeP4(jet_5[i], jet_5[i][4], representation)
 
+    b_runNumber[0]  = i
     
-    b_jet1_px_obs   = jet_1_vect.Px()
-    b_jet1_py_obs   = jet_1_vect.Py()
-    b_jet1_pz_obs   = jet_1_vect.Pz()
-    b_jet1_E_obs    = jet_1_vect.E()
-    b_jet1_m_obs    = jet_1_vect.M()
-    b_jet1_btag_obs   = jet_1[i][5]
-    b_jet2_px_obs   = jet_2_vect.Px()
-    b_jet2_py_obs   = jet_2_vect.Py()
-    b_jet2_pz_obs   = jet_2_vect.Pz()
-    b_jet2_E_obs    = jet_2_vect.E()
-    b_jet2_m_obs    = jet_2_vect.M()
-    b_jet2_btag_obs   = jet_2[i][5]
-    b_jet3_px_obs   = jet_3_vect.Px()
-    b_jet3_py_obs   = jet_3_vect.Py()
-    b_jet3_pz_obs   = jet_3_vect.Pz()
-    b_jet3_E_obs    = jet_3_vect.E()
-    b_jet3_m_obs    = jet_3_vect.M()
-    b_jet3_btag_obs   = jet_3[i][5]
-    b_jet4_px_obs   = jet_4_vect.Px()
-    b_jet4_py_obs   = jet_4_vect.Py()
-    b_jet4_pz_obs   = jet_4_vect.Pz()
-    b_jet4_E_obs    = jet_4_vect.E()
-    b_jet4_m_obs    = jet_4_vect.M()
-    b_jet4_btag_obs   = jet_4[i][5]
-    b_jet5_px_obs   = jet_5_vect.Px()
-    b_jet5_py_obs   = jet_5_vect.Py()
-    b_jet5_pz_obs   = jet_5_vect.Pz()
-    b_jet5_E_obs    = jet_5_vect.E()
-    b_jet5_m_obs    = jet_5_vect.M()
-    b_jet5_btag_obs   = jet_5[i][5]
-    b_jetmu_px_obs   = jet_mu[i][0]
-    b_jetmu_py_obs   = jet_mu[i][1]
-    b_jetmu_pz_obs   = jet_mu[i][2]
-    b_jetmu_T0_obs    = jet_mu[i][3]
-    b_jetlep_ET_obs    = jet_mu[i][4]
-    b_jetlep_phi_obs   = jet_mu[i][5]
+    b_jet1_px_obs[0]   = jet_1_vect.Px()
+    b_jet1_py_obs[0]   = jet_1_vect.Py()
+    b_jet1_pz_obs[0]   = jet_1_vect.Pz()
+    b_jet1_pt_obs[0]   = jet_1_vect.Pt()
+    b_jet1_E_obs [0]   = jet_1_vect.E()
+    b_jet1_m_obs [0]   = jet_1_vect.M()
+    b_jet1_btag_obs[0]   = jet_1[i][5]
+    b_jet2_px_obs[0]   = jet_2_vect.Px()
+    b_jet2_py_obs[0]   = jet_2_vect.Py()
+    b_jet2_pz_obs[0]   = jet_2_vect.Pz()
+    b_jet2_pt_obs[0]   = jet_2_vect.Pt()
+    b_jet2_E_obs [0]   = jet_2_vect.E()
+    b_jet2_m_obs [0]   = jet_2_vect.M()
+    b_jet2_btag_obs[0]   = jet_2[i][5]
+    b_jet3_px_obs[0]   = jet_3_vect.Px()
+    b_jet3_py_obs[0]   = jet_3_vect.Py()
+    b_jet3_pz_obs[0]   = jet_3_vect.Pz()
+    b_jet3_pt_obs[0]   = jet_3_vect.Pt()
+    b_jet3_E_obs [0]   = jet_3_vect.E()
+    b_jet3_m_obs [0]   = jet_3_vect.M()
+    b_jet3_btag_obs[0]   = jet_3[i][5]
+    b_jet4_px_obs[0]   = jet_4_vect.Px()
+    b_jet4_py_obs[0]   = jet_4_vect.Py()
+    b_jet4_pz_obs[0]   = jet_4_vect.Pz()
+    b_jet4_pt_obs[0]   = jet_4_vect.Pt()
+    b_jet4_E_obs [0]   = jet_4_vect.E()
+    b_jet4_m_obs [0]   = jet_4_vect.M()
+    b_jet4_btag_obs[0]   = jet_4[i][5]
+    b_jet5_px_obs[0]   = jet_5_vect.Px()
+    b_jet5_py_obs[0]   = jet_5_vect.Py()
+    b_jet5_pz_obs[0]   = jet_5_vect.Pz()
+    b_jet5_pt_obs[0]   = jet_5_vect.Pt()
+    b_jet5_E_obs [0]   = jet_5_vect.E()
+    b_jet5_m_obs [0]   = jet_5_vect.M()
+    b_jet5_btag_obs[0]   = jet_5[i][5]
+    b_jetmu_px_obs[0]   = jet_mu[i][0]
+    b_jetmu_py_obs[0]   = jet_mu[i][1]
+    b_jetmu_pz_obs[0]   = jet_mu[i][2]
+    b_jetmu_T0_obs [0]   = jet_mu[i][3]
+    b_jetlep_ET_obs [0]   = jet_mu[i][4]
+    b_jetlep_phi_obs[0]   = jet_mu[i][5]
 
     # true
 
@@ -558,3 +584,7 @@ for i in range(n_events):
     b_t_lep_phi_fitted[0] = t_lep_fitted.Phi()
 
     tree.Fill()
+
+tree.Print()
+ofile.Write ()
+ofile.Close ()
